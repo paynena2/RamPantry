20e78a57e5314a126bb72cf941bd3f6c9db5fd86
Nathaniel Payne <paynena2@mymail.vcu.edu>
2019-07-17 21:48:55 -0400

    Fixing food to comply with dependencies

--- src/Food.java
+++ src/Food.java
@@ -1,46 +1,11 @@
 import java.util.Date;
 
+import lombok.Builder;
+import lombok.Value;
+@Value 
+@Builder
 public class Food {
 	String name;
 	int quantity;
 	Date exp;
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getQuantity() {
-		return this.quantity;
-	}
-	
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
-	public Date getExp() {
-		return this.exp;
-	}
-	
-	public void setExp(Date exp) {
-		this.exp = exp;
-	}
-	
-	public String toString() {
-		return this.getName() + "," + this.getQuantity() + "," + this.getExp();
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if(o instanceof Food) {
-			Food other = (Food)o;
-			if((this.getName().equals(other.getName())) && (this.getQuantity() == other.getQuantity()) && this.getExp().equals(other.getExp())) {
-				return true;
-			}
-		}
-		return false;
-	}
 }
