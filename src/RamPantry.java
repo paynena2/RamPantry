c4d5db28914a3b5cdc52eeaa50cea2ae4adc03e9
Nathaniel Payne <paynena2@mymail.vcu.edu>
2019-07-17 21:21:43 -0400

    Added RamPantry methods

--- src/RamPantry.java
+++ src/RamPantry.java
@@ -1,4 +1,26 @@
+import java.util.List;
 
 public class RamPantry {
-	String example;
+	List<Food> foodList;
+	
+	
+	public void addEntry(Food foodItem) {
+		
+	}
+	
+	public void removeEntry(Food foodItem) {
+		
+	}
+	
+	public void printEntries() {
+		
+	}
+	
+	public void loadList() {
+		
+	}
+	
+	public void saveList() {
+		
+	}
 }
