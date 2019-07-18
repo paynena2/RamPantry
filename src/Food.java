c30bd08bf658706d119a46bcebfa2ce417baf4de
Nathaniel Payne <paynena2@mymail.vcu.edu>
2019-07-17 21:27:21 -0400

    Starting food class + testing initial commits

--- src/Food.java
+++ src/Food.java
@@ -1,4 +1,15 @@
+import java.util.Date;
 
 public class Food {
 	String name;
+	int quantity;
+	Date exp;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
